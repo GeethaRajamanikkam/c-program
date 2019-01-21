a8e717870a2baca0b0033aac85143db173213869
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-01-21 07:13:39 -0800

    Create b1.c

---
+++ b1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int a;
+  scanf("%d",&a);
+  if(a>0)
+  {
+printf("Positive number");
+  }
+else if(a<0)
+{
+printf("Negative number");
+}
+else 
+{
+  printf("Zero");
+}
+
+ }
+  
