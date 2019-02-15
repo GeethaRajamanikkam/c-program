3469ed550b0b9a417e4568ea1970e13f6e7c4a9a
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-02-15 20:40:13 +0530

    Create set9b7.c

---
+++ set9b7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+  int a,b,i,n;
+  scanf("%d %d",&a,&b);
+  for(i=1;i<=a&&i<=b;i++)
+  {
+    n=i;
+  }
+  printf("%d",n);
+    return 0;
+}
