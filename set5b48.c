5460b759b4a83c6c0810f5fd3113b5602c1ddf8f
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-02-20 23:21:05 +0530

    Create set5b48.c

---
+++ set5b48.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+ int a[100],n,i,s=0,avg;
+ scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+  s=s+a[i];
+}
+avg=s/n;
+printf("%d",avg);
+return 0;
+}
