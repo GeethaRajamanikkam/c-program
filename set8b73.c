4c587a773f10e56bcd301f342478f3a9615dccdd
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-02-24 18:49:38 +0530

    Create set8b73.c

---
+++ set8b73.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+int a,b,c,i,flag;
+  scanf("%d %d %d",&a,&b,&c);
+  for(i=b;i<=c;i++)
+  {
+    if(a==i)
+    {
+      flag=1;
+      break;
+    }
+    else
+    {
+      flag=0;
+    }
+  }
+  if(flag==1)
+  {
+    printf("yes");
+  }
+  else 
+  {
+    printf("no");
+  }
+  return 0;
+}
