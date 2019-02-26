e527b6c8388574473abdc929a9010f3165eae9a4
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-02-26 16:09:36 +0530

    Create set2p11.c

---
+++ set2p11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  char a[100];
+  int i=0;
+  scanf("%s",a);
+  if(a[i]=='s')
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+
+  return 0;
+}
