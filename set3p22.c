050a68c3670d53d76952d1bc009ef2b6b0bee8d5
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-02-28 18:23:59 +0530

    Create set3p22.c

---
+++ set3p22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int a,b,i,m;
+  scanf("%d %d",&a,&b);
+  for(i=2;i<=a;i++)
+  {
+    if(a%i==0 && b%i==0)
+    {
+    m=i;
+    }
+  }
+  printf("%d",m);
+  return 0;
+}
