116a316341b49b87beed936af7a0fb5c5200b1b2
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-03-06 22:14:08 +0530

    Create set6p52.c

---
+++ set6p52.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[100],n,i,j,t,k;
+  scanf("%d %d",&n,&k);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+  {
+    if(a[i]>a[j])
+    {
+      t=a[i];
+      a[i]=a[j];
+      a[j]=t;
+    }
+  }
+}
+i=k;
+printf("%d",a[i-1]);
+  return 0;
+}
