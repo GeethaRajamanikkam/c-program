ee0632bbf5f4b9ea6da056adfed83c478eb032fe
GeethaRajamanikkam <46813698+GeethaRajamanikkam@users.noreply.github.com>
2019-03-26 22:09:33 +0530

    Create set10p92.c

---
+++ set10p92.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+   int n,b,s=0,m=1,r;
+   scanf("%d",&n);
+   while(n>0)
+   {
+     r=n%10;
+     s=s+r*m;
+     n=n/10;
+     m=m*2;
+   }
+   printf("%d",s);
+    return 0;
+}
